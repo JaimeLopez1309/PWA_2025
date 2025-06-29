e903a4aee09166eb9b66fef4284382cc30119439
JaimeLopez1309 <jaime.lopez-p@uniminuto.edu.co>
2025-06-29 17:06:55 -0500

    Actualización Rutas

--- service_worker.js
+++ service_worker.js
@@ -2,7 +2,7 @@ self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open("pwa-cache").then((cache) => {
             return cache.addAll([
-                "./public/index.html",
+                "./index.html",
                 "./src/styles.css",
                 "./src/app.js",
                 "./assets/logo.png"
