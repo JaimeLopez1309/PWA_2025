2998249124112805e2055122cabda8cc525332f1
JaimeLopez1309 <jaime.lopez-p@uniminuto.edu.co>
2025-06-29 17:19:26 -0500

    Agrego imágenes de productos

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const productos = [
-        { nombre: "Laptop Gamer", precio: "$1200", imagen: "/assets/laptop.jpg" },
-        { nombre: "Smartphone", precio: "$800", imagen: "/assets/smartphone.jpg" },
-        { nombre: "Monitor 4K", precio: "$500", imagen: "/assets/monitor.jpg" }
+        { nombre: "Laptop Gamer", precio: "$1200", imagen: "./assets/laptop.jpg" },
+        { nombre: "Smartphone", precio: "$800", imagen: "./assets/smartphone.jpg" },
+        { nombre: "Monitor 4K", precio: "$500", imagen: "./assets/monitor.jpg" }
     ];
     
     const listaProductos = document.querySelector(".lista-productos");
